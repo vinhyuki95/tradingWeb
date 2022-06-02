bcde973219a91707f742d720b40de738d3277db9
Vinh Le <vinhyuki1995@gmail.com>
2022-06-02 14:49:41 +0700

    responsive-market

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4.8,
-  spaceBetween: 30,
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
+  slidesPerView: 5,
+  breakpoints: {
+    // 1474:{
+    //   slidsPerView: 3.5,
+    // },
+    1477:{
+      slidesPerView: 4.3,
+    },
+    1496: {
+      slidesPerView: 4.5,
+    },
+    
+  },
 });
