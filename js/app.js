82b5d628fc5cf1a3fe5c23d6c5484651e7bf85e4
Vinh Le <vinhyuki1995@gmail.com>
2022-06-03 15:13:17 +0700

    fix-responsive-login

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 5,
   breakpoints: {
-    // 1474:{
-    //   slidsPerView: 3.5,
-    // },
-    1477:{
-      slidesPerView: 4.3,
-    },
     1496: {
       slidesPerView: 4.5,
     },
