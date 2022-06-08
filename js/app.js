7cd0bdc6ffaa1da548de8af4eb1d06f9f877c562
Vinh Le <vinhyuki1995@gmail.com>
2022-06-08 08:17:51 +0700

    js-show-menu

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@ var swiper = new Swiper(".mySwiper", {
     
   },
 });
+
+const logo = document.querySelector(".logo");
+const menuBox = document.querySelector(".menu__box");
+const turnOffMenu = document.querySelector(".fa-solid.fa-xmark ");
+const navHide = document.querySelector(".navHide");
+logo.onclick = function(){
+  if(menuBox.classList.contains("active")){
+    menuBox.classList.remove("active");
+  } else{
+    menuBox.classList.add("active");
+  }
+}
+turnOffMenu.onclick = function(){
+  menuBox.classList.remove("active");
+}
+menuBox.appendChild(navHide);
+ 
+
