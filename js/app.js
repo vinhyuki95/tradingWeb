27746cca23ed470db577e8da2938ee0229ccffec
Vinh Le <vinhyuki1995@gmail.com>
2022-06-13 11:36:37 +0700

    fix-event-account

--- js/app.js
+++ js/app.js
@@ -24,41 +24,6 @@ if(tabBox.classList.contains("active")){
 };
 tabBox.appendChild(navHide);
 
-// selector element (sử lý sự kiện khi click vào account)
-const accountBox = document.querySelector(".account__box");
-const headerAccount=document.querySelector(".header__account");
-// create new element then add a class
-const sideBarMenuContainer = document.createElement("div");
-sideBarMenuContainer.classList.add("account-dropdown");   //bọc chứa content khi click vào tài khoản
-headerAccount.appendChild(sideBarMenuContainer);
-
-accountBox.onclick = function(){
-  if(sideBarMenuContainer.classList.contains("active")){
-    sideBarMenuContainer.classList.remove("active");
-  }else{
-    sideBarMenuContainer.classList.add("active");
-  }
-}
-
-// tạo vòng lặp để chạy qua từng phần tử(cho nội dụng vào account-dropdown)
-const arr= [["../../img/account/overview.png","Tổng quan"],["../../img/account/security.png", "Bảo mật"],["../../img/account/change-pin-code.png","Thay đổi mã pin"],["../../img/account/change-otp.png","Đổi phương thức OTP"],["../../img/account/ic_logout.png","Đăng xuất"]];
-for(let i = 0; i<arr.length; i=i+1){
-  const menuItemContainer = document.createElement("div");
-  menuItemContainer.classList.add("menu-item-container");
-  sideBarMenuContainer.appendChild(menuItemContainer);
-
-  const menuItemLogo= document.createElement("img");
-  menuItemLogo.classList.add("menu-item-logo");
-  menuItemLogo.src= arr[i][0];
-  menuItemContainer.appendChild(menuItemLogo);
-
-  const menuItemName = document.createElement("div");
-  menuItemName.classList.add("menu-item-name");
-  menuItemName.innerHTML = arr[i][1];
-  menuItemContainer.appendChild(menuItemName);
-}
-
-
 // selector deepzoom
 const deepzoomLogo = document.querySelector(".deepzoom");
 const chartWrap = document.querySelectorAll(".chart__wrap");
