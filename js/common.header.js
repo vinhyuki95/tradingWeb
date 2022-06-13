457df0e90f19a87fabac526eb92b1368659f9d66
Vinh Le <vinhyuki1995@gmail.com>
2022-06-13 15:50:55 +0700

    js-code-asset

--- js/common.header.js
+++ js/common.header.js
@@ -32,3 +32,45 @@ for(let i = 0; i<arr.length; i=i+1){
   menuItemName.innerHTML = arr[i][1];
   menuItemContainer.appendChild(menuItemName);
 }
+
+//xử lí sự kiện khi click tài sản
+// selector element được click
+const assetElement = document.querySelector(".asset");
+const headerWrapElement = document.querySelector(".header__wrap");
+const sidebarAssetContainer=document.createElement("div");
+sidebarAssetContainer.classList.add("sidebar-asset-container");
+headerWrapElement.appendChild(sidebarAssetContainer);
+assetElement.onclick = function(){
+  if(sidebarAssetContainer.classList.contains("active")){
+    sidebarAssetContainer.classList.remove("active");
+  }else{
+    sidebarAssetContainer.classList.add("active");
+  }
+}
+
+const arrSidebarAsset=[["../../img/common-header/left-log.svg","Ẩn Menu"],["../../img/common-header/total-assets.png","Tổng tài sản","aa"]];
+console.log(arrSidebarAsset);
+for(let i=0; i<arrSidebarAsset.length; i=i+1){
+  const menuContainer=document.createElement("div");
+  menuContainer.classList.add("menu-container");
+  sidebarAssetContainer.appendChild(menuContainer);
+
+  const itemLogo = document.createElement("div");
+  itemLogo.classList.add("item-logo");
+  itemLogo.src= arrSidebarAsset[i][0];
+  menuContainer.appendChild(itemLogo);
+  console.log(itemLogo);
+
+  const itemNameAsset = document.createElement("div");
+  itemNameAsset.classList.add("item-name");
+  itemNameAsset.innerHTML= arrSidebarAsset[i][1];
+  menuContainer.appendChild(itemNameAsset);
+  console.log(itemNameAsset);
+
+  const dropdownLogo = document.createElement("div");
+  dropdownLogo.classList.add("dropdown-logo");
+  dropdownLogo.innerHTML = arrSidebarAsset[i][2];
+  menuContainer.appendChild(dropdownLogo);
+  console.log(dropdownLogo);
+
+}
